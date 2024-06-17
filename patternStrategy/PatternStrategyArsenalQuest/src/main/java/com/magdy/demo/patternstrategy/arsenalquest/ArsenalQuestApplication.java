299f786a250af60169607d0721dd6293f297b0b4
Magdaly.santos <magdaly.santos@tenpo.cl>
2024-06-17 18:14:06 -0300

    new project ArsenalQuest with pattern strategy

---
+++ patternStrategy/PatternStrategyArsenalQuest/src/main/java/com/magdy/demo/patternstrategy/arsenalquest/ArsenalQuestApplication.java
@@ -0,0 +1,13 @@
+package com.magdy.demo.patternstrategy.arsenalquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArsenalQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArsenalQuestApplication.class, args);
+	}
+
+}
---
+++ patternStrategy/PatternStrategyArsenalQuest/src/test/java/com/magdy/demo/patternstrategy/arsenalquest/ArsenalQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magdy.demo.patternstrategy.arsenalquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternStrategyArsenalQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
